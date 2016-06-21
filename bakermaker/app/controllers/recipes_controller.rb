b294f067a32cf42c4f0fe485d15609ecbf743fa4
xristocodos <xristojobos@gmail.com>
2016-06-21 08:51:26 -0400

    restructuring

--- bakermaker/app/controllers/recipes_controller.rb
+++ bakermaker/app/controllers/recipes_controller.rb
@@ -1,2 +1,36 @@
 class RecipesController < ApplicationController
+  # before_action :select_issue, only: [:show, :edit, :update, :destroy]
+  # before_filter :authorize
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def new
+    @recipe = Recipe.new
+    4.times do
+      @recipe.ingredients.build()
+    end
+  end
+
+  def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to @recipe
+  end
+
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :title,
+      ingredients_attributes: [
+        :name,
+        :quantity
+      ]
+    )
+  end
 end
--- bakermaker/app/models/recipe.rb
+++ bakermaker/app/models/recipe.rb
@@ -1,3 +1,4 @@
 class Recipe < ApplicationRecord
   has_many :ingredients
+  accepts_nested_attributes_for :ingredients, reject_if: :all_blank
 end
