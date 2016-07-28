9f028aacb6fefa73296ca4f3607eb7bb31f13e31
xristocodos <xristojobos@gmail.com>
2016-07-28 15:38:05 -0400

    sorted out routes by purpose, tested successfully for registration token handling

--- bakermaker/config/routes.rb
+++ bakermaker/config/routes.rb
@@ -1,48 +1,44 @@
 Rails.application.routes.draw do
   resources :recipes
   resources :friendships
-
-  get 'registrations/new'
-  get 'registrations/create'
-
-  get 'invitations/new'
-  get 'invitations/create'
-  get 'invitations/destroy'
-
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-
   # resources :ingredients
+
+  ### HOME PAGE (The Jump Off)
   root 'welcome#new'
   get '/home', to: 'welcome#show', as: 'home'
 
 
-  #form 1: login
+  ### LOGIN (form 1)
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy', as: 'logout'
+  get 'sessions/new'
+  # get 'sessions/create'
+  get 'sessions/destroy'
 
-  #form 2: invite
+
+  ### INVITES (form 2)
   get '/invite', to: 'invitations#new', as: 'invite'
   post '/invite', to: 'invitations#create'
   get '/invite_index', to: 'invitations#index', as: 'invitee_index'
+  get 'invitations/new'
+  get 'invitations/create'
+  get 'invitations/destroy'
   #? add delete for revoking invite?#
 
-  #form 3: sign up
-  get '/register/:token', to: 'registrations#new', as: 'register'
-  post '/register', to: 'registration#create', as: 'register_create'
 
+  ### REGISTRATION (form 3)
+  #get /register/(no token), send to error page indicating that a token is required
+  post '/register', to: 'registration#create', as: 'register_create'
+  get '/register/:token', to: 'registrations#new', as: 'register'
+
+  get 'registrations/new'
+  get 'registrations/create'
+
+
+  ### RECIPE (form 4)
   #form 4: create a recipe
   #done
 
 
-  #
-  # #### REFERENCE aka GET RID OF ME
-  #   # Login
-  #   get '/login' => 'sessions#new', as: 'login'
-  #   post '/login' => 'sessions#create', as: 'create_login'
-  #
-  #   # Logout
-  #   delete '/logout' => 'sessions#destroy', as: 'logout'
 end
