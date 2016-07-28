bb5f80402498f3dd3d30bd92701aeb0003a904f4
xristocodos <xristojobos@gmail.com>
2016-07-28 15:33:38 -0400

    added code for including Inviter & Invitee's name in New User Registration form, pulled from invite lookup via token

--- bakermaker/app/controllers/registrations_controller.rb
+++ bakermaker/app/controllers/registrations_controller.rb
@@ -1,16 +1,31 @@
 class RegistrationsController < ApplicationController
   def new
+    #todo: use token to get invited_by_id and set it
+    #todo: use invited_by_id to get inviter's name and set it
+
     @user = User.new
     #how to access invitee info from here?
     #add to view:
     @token = params[:token]
+
+    #info for populating the customized forum
+    @invited = Invitee.find_by(token: @token)
+
+    @inviter_id = @invited[:invited_by_id]
+    @inviter = User.find_by(id: @inviter_id)
   end
 
+
   def create
-    binding.pry
-    if invitee[token] == @token
-      permit_registration
+    if @invited[token] == @token
+      permit_registration == true
     end
+    binding.pry
+
+    if permit_registration
+      ##save registration to db
+    end
+
     #check if incoming token in URL == invitee token
     #if ???params[:t]??? != invitee[:token]
     #  redirect_to register     #redirect to register with warning flash stating that invite is no longer valid
